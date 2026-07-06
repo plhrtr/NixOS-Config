15bf5afd862c94abc2226ad554845e53a932dcd5
Paul Harter <pl.hrtr@gmail.com>
2026-07-06 11:01:56 +0200

    06.07

--- modules/home-manager/lazyvim/lazyvim.nix
+++ modules/home-manager/lazyvim/lazyvim.nix
@@ -1,17 +1,15 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   programs.neovim = {
-    enable = true;
-    extraPackages = with pkgs; [
-      # Linter and formatter
-      statix
-
-      # LSPs
-      lua-language-server
-      nil
-    ];   
     defaultEditor = true;
   };
 
-  xdg.configFile."nvim".source = ./nvim;
+  home.packages = with pkgs; [
+    # Linter and formatter
+    statix
+
+    # LSPs
+    lua-language-server
+    nil
+  ];
 }
